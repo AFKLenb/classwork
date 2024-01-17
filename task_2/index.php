80f0e9a42733319f5af06a8c73c77cdf523ffcdb
AFKLenb <markermma@gmail.com>
2024-01-17 17:01:15 +0300

    Grobovik and Manchesov

--- task_2/index.php
+++ task_2/index.php
@@ -1,9 +1,16 @@
 <?
 
-$count = 23;
+$count = 1;
 //
-if($count >= 18){
-    echo "Он старше 18";
-}else{
-    echo "ему 4";
+if($count == 1){
+    echo "Возьмите зонт, на улице дождь";
+}
+else if($count == 2){
+    echo "На улице идёт снег";
+}
+else if($count == 3){
+    echo "На улице жарко";
+}
+else{
+    echo "На улице опасно";
 }
